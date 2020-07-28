119eb9bef4e0aed71d0e55c14cba235be9c7301f
reoxb <suarzs@gmail.com>
2020-07-28 15:32:00 -0500

    Fix eslint things

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js'
   },
   output: {
-    publicPath: "/dist/",
+    path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map',
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
       {
         test: /\.(css|sass|scss)$/,
         use: [
@@ -52,35 +60,25 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        enforce: 'pre', // checked before being processed by babel-loader
-        test: /\.(js|jsx)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
     ]
   },
   // DevServer -> https://webpack.js.org/configuration/dev-server/
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+    port: 9000,
+    open: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "index.html",
